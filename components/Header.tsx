e7458a3d12fe1ea8dfa57056a4114b9d118d9f25
Phuong Vu <phuong.vu@bluebottle.digital>
2024-04-26 16:50:11 +0700

    (+) update header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+
+import Image from 'next/image';
+import LogoSrc from '@public/images/logo-light.png';
+
+export const Header = () => {
+	return (
+		<section className='flex justify-between'>
+			<Image src={LogoSrc} alt='logo'/>
+			<div></div>
+		</section>
+	)
+}
